6ab8c5cbbde633046b2d9e28fa52324851bedcb4
Ben Warner <benjaminnwarner@gmail.com>
2018-06-26 20:14:22 -0400

    Basic logic for validation

--- src/components/EmailPhoneEntry.vue
+++ src/components/EmailPhoneEntry.vue
@@ -17,7 +17,36 @@ export default {
   },
   methods: {
     submitSearch: function(){
-      console.log(this.emailOrPhone);
+      var searchQuery = this.emailOrPhone;
+      if(this.isValidPhone(searchQuery)){
+        //var phoneParams = this.formatUrlParams(searchQuery);
+        this.getRecovery();
+      }
+      else if(this.isValidEmail(searchQuery)){
+        //var emailParams = this.formatUrlParams(searchQuery);
+        this.getRecovery();
+      }
+    },
+
+    formatUrlParams(payload){
+      var params = Object.entries(payload).map(e => e.join('=')).join('&');
+      return '?' + params;
+    },
+
+    isValidPhone(phoneNumber){
+      // eslint-disable-next-line
+      var phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+      return phoneRegex.test(phoneNumber);
+    },
+
+    isValidEmail(emailAddress){
+      // eslint-disable-next-line
+      var emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return emailRegex.test(String(emailAddress).toLowerCase());
+    },
+
+    getRecovery(){
+
     }
   }
 }
